a161beab7f5ee8657a7568f8bf056d8abf4cc880
charming-reference-7654 <sydeffect1994@gmail.com>
2019-06-01 21:56:19 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,11 +1,9 @@
 var a = 10
 var b = 20
 
-beforeEach(function() {
-  a = Math.floor(Math.random() * 1000)
-  b = Math.floor(Math.random() * 1000)
-})
-
-function add(){
+function add() {
   return a + b
+  
 }
+
+add()
